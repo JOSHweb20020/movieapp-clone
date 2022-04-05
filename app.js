dca66f368e0f6d9b60e5c8d67a5d38602e98bc7b
JOSHweb20020 <103070641+JOSHweb20020@users.noreply.github.com>
2022-04-05 22:59:27 +0100

    remake movieapp
    help get the error and fix . with appropriate explanation on how it was fixed . i'm a young developer and thats the only way i can learn properly

---
+++ app.js
@@ -0,0 +1,75 @@
+const API_KEY = 'd48f331a6b75ea11ef5379196afb31c5';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=d48f331a6b75ea11ef5379196afb31c5';
+console.log('This is my Movie app')
+
+
+const moviesearchable = document.querySelector('#moviesearchable');
+const inputElement = document.querySelector('#inputvalue');
+const buttonElement = document.querySelector('#search');
+
+
+function movieSection (movies){`
+return ${movies.map((movie) => {
+    return`
+     <img src="${movie.poster_path}" data-movie-id="${movie.id}/>`
+})}
+
+`
+}
+
+
+function createMovieContainer(movies){
+    const movieElement = document.createElement('div')
+    movieElement.setAttribute('class', 'movie')
+
+    const movieTemplate = `
+   
+    <section class="section">
+    ${movieSection(movies)}
+    </section>
+    <div class="content content-display">
+        <p class="content-close"></p>
+    </div>
+    `
+    movieElement.innerHTML = movieTemplate
+    return movieTemplate
+}
+
+
+
+buttonElement.onclick = function(event){
+    event.preventDefault();
+    const value = inputElement.value;
+    
+
+    const newUrl = url + '&query=' + value
+
+    fetch(newUrl)
+    .then((res) => res.json())
+    .then((data) => {
+
+        //data.results
+
+        const movies = data.results
+        
+        const movieBlock = createMovieContainer(movies)
+        moviesearchable.body.appendChild(movieBlock);
+        console.log('Data', data)
+
+
+      
+    })
+    .catch((error) => {
+        console.log('Error', error)
+    })
+
+    console.log('value' , value)
+
+
+}
+
+
+
